3cd27dea4a7c564869b620baffc08e95c78ad58e
MatteusNogueira <matteus.bins@sptech.school>
2024-12-09 20:15:14 -0300

    subindo novos logs console

--- front_end/react/src/website/pages/Login/Login.jsx
+++ front_end/react/src/website/pages/Login/Login.jsx
@@ -5,6 +5,8 @@ import Header from "../../components/Header";
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_CLOUD_API_URL;
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_CLOUD_API_URL);
 console.log(`${apiUrl}/auth/login`)
 
 function Login() {
